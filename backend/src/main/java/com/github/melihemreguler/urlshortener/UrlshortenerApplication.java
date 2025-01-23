20328119780987265ae2b2c161dc7a18dc47f41a
MelihEmreGuler <gulermelihemre@gmail.com>
2025-01-23 23:38:10 +0300

    Initial commit

---
+++ backend/src/main/java/com/github/melihemreguler/urlshortener/UrlshortenerApplication.java
@@ -0,0 +1,16 @@
+package com.github.melihemreguler.urlshortener;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerApplication.class, args);
+	}
+
+}
+
+
