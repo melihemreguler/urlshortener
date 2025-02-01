492b575cf0b84d0a9ac5fd6f0d8d0186e5af1bbe
MelihEmreGuler <gulermelihemre@gmail.com>
2025-02-01 15:37:07 +0300

    fix typo

--- backend/src/main/java/com/github/melihemreguler/urlshortener/UrlshortenerApplication.java
+++ backend/src/main/java/com/github/melihemreguler/urlshortener/UrlshortenerApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlshortenerApplication {
+public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrlshortenerApplication.class, args);
+		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
 
 }
