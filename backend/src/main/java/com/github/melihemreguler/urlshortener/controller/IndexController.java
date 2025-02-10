fab19797f3f67ca59f87233116ed5decb592a880
MelihEmreGuler <gulermelihemre@gmail.com>
2025-02-10 23:30:54 +0300

    add Index Controller to redirect swagger ui

---
+++ backend/src/main/java/com/github/melihemreguler/urlshortener/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.github.melihemreguler.urlshortener.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@Slf4j
+public class IndexController {
+
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        log.info("Redirecting to Swagger UI");
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/swagger-ui/index.html#/");
+        return redirectView;
+    }
+}
