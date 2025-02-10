29fa49d2a3024207e46ccb98ae26ddd4e3998a7e
MelihEmreGuler <gulermelihemre@gmail.com>
2025-02-10 23:21:15 +0300

    rename model to dto

--- backend/src/main/java/com/github/melihemreguler/urlshortener/repository/UrlRepository.java
+++ backend/src/main/java/com/github/melihemreguler/urlshortener/repository/UrlRepository.java
@@ -1,13 +1,13 @@
 package com.github.melihemreguler.urlshortener.repository;
 
-import com.github.melihemreguler.urlshortener.model.Url;
+import com.github.melihemreguler.urlshortener.dto.UrlDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UrlRepository extends MongoRepository<Url, String> {
-    Optional<Url> findByPath(String url);
-    Optional<Url> findByShortCode(String code);
+public interface UrlRepository extends MongoRepository<UrlDto, String> {
+    Optional<UrlDto> findByLongUrl(String longUrl);
+    Optional<UrlDto> findByShortCode(String code);
 }
